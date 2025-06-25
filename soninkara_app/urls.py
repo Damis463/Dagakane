532fc2674a8dd6aba2a897d7fd26fed55d2b44ae
Adams463 <st748243@gmail.com>
2025-06-25 15:09:27 +0200

    Initial commit Soninkara

--- soninkara_app/urls.py
+++ soninkara_app/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     # Interface d'administration Django
     path('admin/', admin.site.urls),
